756a003ca0633b7ce7ceb8f9da7cf8e9e74d5f31
JheanMarllos <jhean@stop-play.com>
2019-09-02 15:08:47 -0300

    Update settings.py

--- classrooms/classrooms/settings.py
+++ classrooms/classrooms/settings.py
@@ -150,7 +150,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'matricula@agendatellme.com.br'
-EMAIL_HOST_PASSWORD = 'tellmematricula'
+EMAIL_HOST_PASSWORD = 'atwork708'
 EMAIL_PORT = 587
 
 
