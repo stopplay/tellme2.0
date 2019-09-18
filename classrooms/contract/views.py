4291c6691020e44a1fa18846c1ec020aa8705d3c
JheanMarllos <jhean@stop-play.com>
2019-09-18 15:10:55 -0300

    Update views.py

--- classrooms/contract/views.py
+++ classrooms/contract/views.py
@@ -761,17 +761,12 @@ def createacontract_rest(request):
 
 @login_required
 def mark_slm_purchased(request, contract_id):
-	contract = Contract.objects.get(contract_id=contract_id)
-
-	# first_auth_signe
-	# second_auth_signe
-	# student_auth_signe
-
-	if contract:
-		contract.purchased_slm = True
-		contract.save()
-
-	return redirect('/contracts/all')
+    contract = Contract.objects.get(contract_id=contract_id)
+    if contract:
+        contract.purchased_slm = True
+        contract.save()
+    messages.success(request, 'O material foi marcado como comprado.')
+    return redirect('/contracts/all')
 
 @login_required
 def set_contract_paid(request, contract_id):
@@ -1382,7 +1377,7 @@ def set_signed_rest(request, contract_id = None):
 def delete_contract(request, contract_id = None):
 	contract_to_delete = Contract.objects.get(contract_id=contract_id)
 	contract_to_delete.delete()
-	messages.success(request, 'Este contrato foi deletado com sucesso!')
+	messages.success(request, 'Contrato {} foi deletado com sucesso!'.format(contract_id))
 	return redirect('/contracts/all')
 
 def seefinancialdetails(request, contract_id = None):
