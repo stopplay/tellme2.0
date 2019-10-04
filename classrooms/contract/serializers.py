3c3243e96884d76aec030bb6c66d580cd40727ea
JheanMarllos <jhean@stop-play.com>
2019-10-04 08:37:34 -0300

    Update serializers.py

--- classrooms/contract/serializers.py
+++ classrooms/contract/serializers.py
@@ -6,10 +6,11 @@ from block.serializers import *
 class ContractSerializer(serializers.ModelSerializer):
     first_auth_signe = ParentSerializer(required=False)
     second_auth_signe = ParentSerializer(required=False)
+    third_auth_signe = ParentSerializer(required=False)
     first_witness_signe = WitnessSerializer(required=False)
     second_witness_signe = WitnessSerializer(required=False)
     counter_signe = SupervisorSerializer(required=False)
     chain = ChainSerializer()
     class Meta:
         model = Contract
-        exclude = ['third_auth_signe']
+        fields = '__all__'
