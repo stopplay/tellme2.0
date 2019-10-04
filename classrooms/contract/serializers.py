d2ed37be8fac656e30d3b7bde6b89914341338a1
JheanMarllos <jhean@stop-play.com>
2019-10-04 09:26:23 -0300

    Update serializers.py

--- classrooms/contract/serializers.py
+++ classrooms/contract/serializers.py
@@ -7,6 +7,7 @@ class ContractSerializer(serializers.ModelSerializer):
     first_auth_signe = ParentSerializer(required=False)
     second_auth_signe = ParentSerializer(required=False)
     third_auth_signe = ParentSerializer(required=False)
+    student_auth_signe = StudentSerializer(required=False)
     first_witness_signe = WitnessSerializer(required=False)
     second_witness_signe = WitnessSerializer(required=False)
     counter_signe = SupervisorSerializer(required=False)
