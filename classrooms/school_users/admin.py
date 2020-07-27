89993070dca684f5adfa508f68455c36825788a0
JheanMarllos <jhean@stop-play.com>
2020-07-27 14:21:21 -0300

    Update admin.py

--- classrooms/school_users/admin.py
+++ classrooms/school_users/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class UserAdmin(admin.ModelAdmin):
-	list_display = ['id', 'username']
-
-admin.site.register(User, UserAdmin)
-
 class HeadAdmin(admin.ModelAdmin):
 	list_display = ['head_id','name', 'profile']
 
