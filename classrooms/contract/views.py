e280561e7601307334734530f6e8050858d205da
JheanMarllos <jhean@stop-play.com>
2020-07-27 12:45:13 -0300

    Update views.py

--- classrooms/contract/views.py
+++ classrooms/contract/views.py
@@ -377,8 +377,8 @@ def createacontract(request):
         for school in School.objects.all():
             for classe in school.classes.all():
                 if student in classe.students.all():
-                    chains += [(classe.class_id)]
-        form.fields["chain"].queryset = Chain.objects.filter(id__in=chains)
+                    chains += [("{0}-{1}-{2}-{3}".format(school.school_name, classe.enrollment_class_year, classe.class_unit, classe.class_name))]
+        form.fields["chain"].queryset = Chain.objects.filter(name__in=chains)
         if request.method == 'POST':
             if form.is_valid():
                 wish = request.POST.get('wish' or None)
