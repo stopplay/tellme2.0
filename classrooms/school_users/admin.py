e443274bbbfe352dc3e802d68ef70b79ff018382
JheanMarllos <48156441+JheanMarllos@users.noreply.github.com>
2020-09-03 09:51:02 -0300

    Update admin.py

--- classrooms/school_users/admin.py
+++ classrooms/school_users/admin.py
@@ -9,30 +9,36 @@ admin.site.register(Head, HeadAdmin)
 
 class TeacherAdmin(admin.ModelAdmin):
 	list_display = ['teacher_id','name', 'profile']
+	search_fields = ('name',)
 
 admin.site.register(Teacher, TeacherAdmin)
 
 class AdminAdmin(admin.ModelAdmin):
 	list_display = ['admin_id','name', 'profile']
+	search_fields = ('name',)
 
 admin.site.register(Admin, AdminAdmin)
 
 class SupervisorAdmin(admin.ModelAdmin):
 	list_display = ['supervisor_id','name', 'profile']
+	search_fields = ('name',)
 
 admin.site.register(Supervisor, SupervisorAdmin)
 
 class ParentAdmin(admin.ModelAdmin):
 	list_display = ['parent_id','name', 'profile']
+	search_fields = ('name',)
 
 admin.site.register(Parent, ParentAdmin)
 
 class StudentAdmin(admin.ModelAdmin):
 	list_display = ['student_id','name', 'profile']
+	search_fields = ('name',)
 
 admin.site.register(Student, StudentAdmin)
 
 class WitnessAdmin(admin.ModelAdmin):
 	list_display = ['witness_id','name', 'profile']
+	search_fields = ('name',)
 
 admin.site.register(Witness, WitnessAdmin)
