07c6801040edd7dd0fbefc9e217bedc6f9f6843a
JheanMarllos <48156441+JheanMarllos@users.noreply.github.com>
2020-09-15 18:02:19 -0300

    Update views.py

--- classrooms/contract/views.py
+++ classrooms/contract/views.py
@@ -403,7 +403,7 @@ def createacontract(request):
                 time = request.POST.get('time' or None)
                 if students:
                     for student in students:
-                        result = tasks.create_contract.delay(form.cleaned_data.get('chain').id, form.cleaned_data.get('date'), form.cleaned_data.get('end_date'), form.cleaned_data.get('pdf'), form.cleaned_data.get('terms_of_contract'), form.cleaned_data.get('terms_of_contract_2'), wish, wish_today, student.student_id, selected_user_head, date, time, current_site.domain, 'admin')
+                        result = tasks.create_contract.delay(form.cleaned_data.get('chain').id, form.cleaned_data.get('date'), form.cleaned_data.get('end_date'), form.cleaned_data.get('pdf').read(), form.cleaned_data.get('terms_of_contract'), form.cleaned_data.get('terms_of_contract_2'), wish, wish_today, student.student_id, selected_user_head, date, time, current_site.domain, 'admin')
                     messages.success(request, 'Contrato criado com sucesso!')
                     return redirect('/contracts/all/')
                 else:
