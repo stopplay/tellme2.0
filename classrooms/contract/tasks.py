53dc52bfc6234c414fca498509a912e5f63b946f
JheanMarllos <48156441+JheanMarllos@users.noreply.github.com>
2020-09-15 22:25:48 -0300

    Update tasks.py

--- classrooms/contract/tasks.py
+++ classrooms/contract/tasks.py
@@ -495,7 +495,7 @@ def create_contract(contract, chain_id, wish, wish_today, student_id, head_id, s
             contract.student_id = student.student_id
             if wish == 'sim':
                 if wish_today == 'sim':
-                    print ('Cheguei aqui')
+                    print ('Cheguei aqui: {}-{}'.format(contract.date, contract.end_date))
                     contract.save()
                     print ('Cheguei aqui 2')
                     contract.pdf.save('{}-{}.pdf'.format(contract.name, contract.contract_id), File(pdf_file))
@@ -514,7 +514,7 @@ def create_contract(contract, chain_id, wish, wish_today, student_id, head_id, s
                         return 'Você não informou a data em que o contrato será enviado!'
             else:
                 if wish_today == 'sim':
-                    print ('Cheguei aqui')
+                    print ('Cheguei aqui: {}-{}'.format(contract.date, contract.end_date))
                     contract.save()
                     print ('Cheguei aqui 2')
                     contract_rest = ContractSerializer(contract)
@@ -523,7 +523,7 @@ def create_contract(contract, chain_id, wish, wish_today, student_id, head_id, s
                 elif wish_today == 'não':
                     if sent_date and sent_time:
                         contract.sent_date = datetime.datetime(int(date.split('-')[0]), int(date.split('-')[1]), int(date.split('-')[2]), int(time.split(':')[0]), int(time.split(':')[1]), 00)
-                        print ('Cheguei aqui')
+                        print ('Cheguei aqui: {}-{}'.format(contract.date, contract.end_date))
                         contract.save()
                         print ('Cheguei aqui 2')
                         contract.pdf.save('{}-{}.pdf'.format(contract.name, contract.contract_id), File(pdf_file))
@@ -539,7 +539,7 @@ def create_contract(contract, chain_id, wish, wish_today, student_id, head_id, s
         contract.student_id = student.student_id
         if wish == 'sim':
             if wish_today == 'sim':
-                print ('Cheguei aqui')
+                print ('Cheguei aqui: {}-{}'.format(contract.date, contract.end_date))
                 contract.save()
                 print ('Cheguei aqui 2')
                 contract.pdf.save('{}-{}.pdf'.format(contract.name, contract.contract_id), File(pdf_file))
@@ -549,7 +549,7 @@ def create_contract(contract, chain_id, wish, wish_today, student_id, head_id, s
             elif wish_today == 'não':
                 if sent_date and sent_time:
                     contract.sent_date = datetime.datetime(int(date.split('-')[0]), int(date.split('-')[1]), int(date.split('-')[2]), int(time.split(':')[0]), int(time.split(':')[1]), 00)
-                    print ('Cheguei aqui')
+                    print ('Cheguei aqui: {}-{}'.format(contract.date, contract.end_date))
                     contract.save()
                     print ('Cheguei aqui 2')
                     contract.pdf.save('{}-{}.pdf'.format(contract.name, contract.contract_id), File(pdf_file))
@@ -560,7 +560,7 @@ def create_contract(contract, chain_id, wish, wish_today, student_id, head_id, s
                     return 'Você não informou a data em que o contrato será enviado!'
         else:
             if wish_today == 'sim':
-                print ('Cheguei aqui')
+                print ('Cheguei aqui: {}-{}'.format(contract.date, contract.end_date))
                 contract.save()
                 print ('Cheguei aqui 2')
                 contract.pdf.save('{}-{}.pdf'.format(contract.name, contract.contract_id), File(pdf_file))
@@ -570,7 +570,7 @@ def create_contract(contract, chain_id, wish, wish_today, student_id, head_id, s
             elif wish_today == 'não':
                 if sent_date and sent_time:
                     contract.sent_date = datetime.datetime(int(date.split('-')[0]), int(date.split('-')[1]), int(date.split('-')[2]), int(time.split(':')[0]), int(time.split(':')[1]), 00)
-                    print ('Cheguei aqui')
+                    print ('Cheguei aqui: {}-{}'.format(contract.date, contract.end_date))
                     contract.save()
                     print ('Cheguei aqui 2')
                     contract.pdf.save('{}-{}.pdf'.format(contract.name, contract.contract_id), File(pdf_file))
