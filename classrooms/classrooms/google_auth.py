3a84c36d8919381f0151f74e6ce0dc8875d03b62
JheanMarllos <48156441+JheanMarllos@users.noreply.github.com>
2020-09-28 12:04:00 -0300

    Update google_auth.py

--- classrooms/classrooms/google_auth.py
+++ classrooms/classrooms/google_auth.py
@@ -35,7 +35,7 @@ class google_auth:
                 self.CLIENT_SECRET_FILE, self.SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
+            with open(credential_path, 'wb') as token:
                 pickle.dump(creds, token)
 
         service = build('drive', 'v3', credentials=creds)
