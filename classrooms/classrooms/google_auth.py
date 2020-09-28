8180b9660706df34d1c7783c2902c148625989ac
JheanMarllos <48156441+JheanMarllos@users.noreply.github.com>
2020-09-28 12:24:02 -0300

    Update google_auth.py

--- classrooms/classrooms/google_auth.py
+++ classrooms/classrooms/google_auth.py
@@ -17,26 +17,39 @@ class google_auth:
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
+        print ('Start Getting Credentials')
         cwd_dir = os.getcwd()
+        print ('CWD_dir get')
         credential_dir = os.path.join(cwd_dir, '.credentials')
+        print ('Credentials dir Get')
         if not os.path.exists(credential_dir):
+            print ('Credentials dir doesn\'t existss, create that')
             os.makedirs(credential_dir)
+        print ('Credentials path Get based in the current user in the session')
         credential_path = os.path.join(credential_dir,
                                     self.user.username + '.json')
         if os.path.getsize(credential_path) > 0:
             with open(credential_path, 'rb') as token:
+                print ('Credentials path already exists and is fullfilled, so store credentials')
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
+        print ('Credentials doesn\'t exists or creds not valid, so create new credentials')
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
+                print ('Credentials not valid, so Refresh it')
                 creds.refresh(Request())
             else:
+                print ('Credentials doesn\'t exists so do a new ')
                 flow = InstalledAppFlow.from_client_secrets_file(
                 self.CLIENT_SECRET_FILE, self.SCOPES)
+                print('InstalledAppFlow instance created to run the flow')
                 creds = flow.run_local_server(port=0)
+                print('Flow run and credentials get')
             # Save the credentials for the next run
             with open(credential_path, 'wb') as token:
+                print('Credentials stored in the json')
                 pickle.dump(creds, token)
-
+        print('Service based on Credentials created')
         service = build('drive', 'v3', credentials=creds)
+        print('Service returned to execute the upload file process')
         return service
