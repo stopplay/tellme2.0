80d9408baa5e59cb843ed0d6339a04989dc0af3a
JheanMarllos <48156441+JheanMarllos@users.noreply.github.com>
2020-09-29 11:14:09 -0300

    Update google_auth.py

--- classrooms/classrooms/google_auth.py
+++ classrooms/classrooms/google_auth.py
@@ -50,6 +50,7 @@ class google_auth:
                     access_type='offline',
                     # Enable incremental authorization. Recommended as a best practice.
                     include_granted_scopes='true')
+                print type(authorization_url)
                 return authorization_url
                 print('Flow run and credentials get')
             # Save the credentials for the next run
