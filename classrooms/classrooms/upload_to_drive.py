1a45d4b26a7cee8c389da11b75681ed49349cb0d
JheanMarllos <48156441+JheanMarllos@users.noreply.github.com>
2020-09-29 11:03:27 -0300

    Update upload_to_drive.py

--- classrooms/classrooms/upload_to_drive.py
+++ classrooms/classrooms/upload_to_drive.py
@@ -9,7 +9,6 @@ from oauth2client.file import Storage
 from apiclient.http import MediaFileUpload
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from classrooms import google_auth
-from django.shortcuts import redirect
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
@@ -20,7 +19,7 @@ APPLICATION_NAME = 'Drive API Python Quickstart'
 def get_or_create_drive_folder(user):
     authInst = google_auth.google_auth(user, SCOPES, CLIENT_SECRET_FILE, APPLICATION_NAME)
     drive_service = authInst.get_credentials()
-    if isinstance(drive_service, redirect):
+    if not isinstance(drive_service, google.oauth2.credentials.Credentials):
         return drive_service
 
     response = drive_service.files().list(q="mimeType = 'application/vnd.google-apps.folder'",
